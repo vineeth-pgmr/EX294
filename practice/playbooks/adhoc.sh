285b04c7061a9d471fb6c8a3c5c0c7224b23fe4f
Vineeth Pokkavayalil <41360403+vineeth-pgmr@users.noreply.github.com>
2021-04-04 11:43:38 +0530

    Second Commit

---
+++ practice/playbooks/adhoc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ansible all -m yum_repository -a "name=AppStream baseurl=ftp://192.168.56.115/rhel8/AppStream description='yum for AppStream' gpgcheck=yes gpgkey=ftp://192.168.56.115/rhel8/RPM-GPG-KEY-redhat-release enabled=yes"
+
+ansible all -m yum_repository -a "name=BaseOS baseurl=ftp://192.168.56.115/rhel8/BaseOS description='yum for BaseOS' gpgcheck=yes gpgkey=ftp://192.168.56.115/rhel8/RPM-GPG-KEY-redhat-release enabled=yes" 
